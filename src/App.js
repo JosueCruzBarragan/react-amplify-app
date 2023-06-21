e123dd56309c61f9be46ae31ccf58973423fcc6f
JosueCruzBarragan <jcruz@spsolutions.com.mx>
2023-06-21 16:17:11 -0600

    feat: Provando cambios desde release

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola desde AWS Amplify</h1>
+        <h1>Hola desde release/1.0.0</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
