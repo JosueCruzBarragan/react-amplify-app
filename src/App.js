2c3f1365fdd070bd1ae18d0733da1f7c90750eb9
JosueCruzBarragan <jcruz@spsolutions.com.mx>
2023-06-29 16:17:18 -0600

    Revert "se agrego variable amplify"
    This reverts commit 7c17334ec7f5397e7806a0f1023f1013d1c85e05.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
         <h1>Hola desde dev Amplify test</h1>
         <h1>var: ${process.env.REACT_APP_AMPLIFY_VAR}</h1>
 
+=======
+        <h1>Hola desde estoy en dev</h1>
+>>>>>>> parent of 7c17334 (se agrego variable amplify)
         <a
           className="App-link"
           href="https://reactjs.org"
